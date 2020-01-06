b994cb171bf57332dcf8ead079e4791618b08ede
Oscar Mugambi <oscarmugambi@gmail.com>
2020-01-06 14:47:12 +0300

    updated Shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import HomePage from "./Components/Homepage";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./Pages/Homepage/homepage";
+import ShopPage from "./Pages/Shop/ShopPage";
 import "./App.css";
+import Header from "./Components/Header/Header";
 
+const hats = () => <h1>hats</h1>;
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
