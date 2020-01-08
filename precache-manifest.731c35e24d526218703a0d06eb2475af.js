0e6b96830464b54a0826260048b9ce9549e66263
Oscar Mugambi <oscarmugambi@gmail.com>
2020-01-08 13:58:38 +0300

    Updates

---
+++ precache-manifest.731c35e24d526218703a0d06eb2475af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a9bcdc6ab30b2c4ef251902bca48d6",
+    "url": "/clothing-store/index.html"
+  },
+  {
+    "revision": "2dd673ef2a5d07c3663e",
+    "url": "/clothing-store/static/css/main.93bca21c.chunk.css"
+  },
+  {
+    "revision": "3364d540889995da0c5f",
+    "url": "/clothing-store/static/js/2.7ced02db.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/clothing-store/static/js/2.7ced02db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2dd673ef2a5d07c3663e",
+    "url": "/clothing-store/static/js/main.dc876b9a.chunk.js"
+  },
+  {
+    "revision": "81f2a94d159deb30eda1",
+    "url": "/clothing-store/static/js/runtime-main.dab3a11a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing-store/static/media/crown.ed28accb.svg"
+  }
+]);
